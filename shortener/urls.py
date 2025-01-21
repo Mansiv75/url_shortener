4af4969d8bf873576cbfb956d779c5e303768bf6
Mansiv75 <mv072004@gmail.com>
2025-01-21 23:43:36 +0530

    url redirect

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import shorten_url
+from .views import shorten_url, redirect_to_original
 
 urlpatterns = [
     path('', shorten_url, name='shorten_url'),
+    path('<str:shortened_url>/', redirect_to_original, name='redirect_to_original'),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -3,9 +3,10 @@ import string
 from .models import URL
 import redis
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
 from django.conf import settings
+from django.utils.timezone import now
 
 redis_client = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
 
@@ -29,6 +30,17 @@ def shorten_url(request):
 
             # save to redis for fast lookup
             redis_client.set(shortened_url, original_url)
+            redis_client.set(f"{shortened_url}:hits",0) #initialize it to 0
+            redis_client.set(f"{shortened_url}:last_accessed", "Never") #initialize it to never
 
         return JsonResponse({'shortened_url': shortened_url})
     return JsonResponse(request, 'shortener/shorten_url.html')
+
+def redirect_to_original(request, shortened_url):
+    original_url = redis_client.get(shortened_url)
+    if original_url:
+        redis_client.incr(f"{shortened_url}:hits") #increment hit counter
+        redis_client.set(f"{shortened_url}:last_accessed", now().isoformat()) #update last accessed IP
+        return redirect(original_url)
+    else:
+        return HttpResponse("URL not found", status=404)
